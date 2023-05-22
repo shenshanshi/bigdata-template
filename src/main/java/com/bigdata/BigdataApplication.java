8f4ae18d84c6adf447c85863bdda3b936ff0a227
nico <2265364722@qq.com>
2023-05-22 09:50:45 +0800

    feat(init-005): 打印项目启动成功

--- src/main/java/com/bigdata/BigdataApplication.java
+++ src/main/java/com/bigdata/BigdataApplication.java
@@ -8,6 +8,7 @@ public class BigdataApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BigdataApplication.class, args);
+        System.out.println("-----------------  项目启动成功  -----------------");
     }
 
 }
