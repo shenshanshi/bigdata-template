3ed0decf75f0e14c0b76a31feff89bae2fc65eb3
nico <2265364722@qq.com>
2023-05-22 10:12:45 +0800

    chore(init-019): 测试继承web层通用数据处理

--- src/main/java/com/bigdata/controller/TestController.java
+++ src/main/java/com/bigdata/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.bigdata.controller;
 
+import com.bigdata.common.controller.BaseController;
 import com.bigdata.common.domain.AjaxResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
-public class TestController {
+public class TestController extends BaseController {
 
     @GetMapping("/hello")
     public AjaxResult hello(){
         return AjaxResult.success("hello bigdata!");
     }
+
 }
